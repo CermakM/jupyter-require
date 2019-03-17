75dac50376ed3834655fb0b2418947a458f7b22a
Marek Cermak <macermak@redhat.com>
2019-03-17 23:09:15 +0100

    Bump version
    0.1.12 -> 0.1.13
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   jupyter_require/__about__.py

--- jupyter_require/__about__.py
+++ jupyter_require/__about__.py
@@ -9,7 +9,7 @@ __title__ = "jupyter-require"
 __summary__ = "Jupyter magic for managing linked JavaScript scripts and CSS styles."
 __uri__ = "https://github.com/CermakM/jupyter-require"
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 __author__ = "Marek Cermak"
 __email__ = "macermak@redhat.com"
